f3566ae54eaeb50f67b4cbe79cfebef36fe0928c
Jeff T <tarutmauktoe@gmail.com>
2017-10-16 14:40:29 -0500

    Add salting to user passwords

--- hashutils.py
+++ hashutils.py
@@ -1,9 +1,18 @@
 import hashlib
+import random
+import string
 
-def make_pw_hash(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
+def make_salt():
+    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
+
+def make_pw_hash(password, salt=None):
+    if not salt:
+        salt = make_salt()
+    hash = hashlib.sha256(str.encode(password + salt)).hexdigest()
+    return '{0},{1}'.format(hash, salt)
 
 def password_matches_hash(password, pw_hash):
-    if make_pw_hash(password) == pw_hash:
+    salt = pw_hash.split(',')[1]
+    if make_pw_hash(password, salt) == pw_hash:
         return True
     return False
